5018903ccb8b49bf52a303878a413164e803888f
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-29 21:21:36 +0900

    Add tooltip for showing full text

--- lib/clipcellar/tree_view.rb
+++ lib/clipcellar/tree_view.rb
@@ -15,15 +15,16 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 require "gtk3"
+require "erb"
 
 module Clipcellar
   class TreeView < Gtk::TreeView
-    KEY_COLUMN, TEXT_COLUMN, TIME_COLUMN, STRFTIME_COLUMN, LINE_COLUMN = *0..4
+    KEY_COLUMN, TEXT_COLUMN, TIME_COLUMN, STRFTIME_COLUMN, LINE_COLUMN, ESCAPED_TEXT_COLUMN = *0..5
 
     def initialize(records)
       super()
       @records = records
-      @model = Gtk::ListStore.new(String, String, Time, String, String)
+      @model = Gtk::ListStore.new(String, String, Time, String, String, String)
       create_tree(@model)
     end
 
@@ -70,6 +71,7 @@ module Clipcellar
       self.search_column = TEXT_COLUMN
       self.enable_search = false
       self.rules_hint = true
+      self.tooltip_column = ESCAPED_TEXT_COLUMN
 
       selection.set_mode(:browse)
 
@@ -85,7 +87,7 @@ module Clipcellar
       column.add_attribute(renderer, :text, STRFTIME_COLUMN)
 
       column = Gtk::TreeViewColumn.new
-      column.title = "Text (partial)"
+      column.title = "Part of a Text (the Full Text appears in a Tooltip)"
       append_column(column)
       renderer = Gtk::CellRendererText.new
       column.pack_start(renderer, :expand => false)
@@ -101,6 +103,8 @@ module Clipcellar
       iter.set_value(TIME_COLUMN, record[:time])
       iter.set_value(STRFTIME_COLUMN, record[:time].strftime("%Y-%m-%d %H:%M:%S"))
       iter.set_value(LINE_COLUMN, record[:text].gsub(/[\n\t]/, " "))
+      escaped_text = ERB::Util.html_escape(record[:text])
+      iter.set_value(ESCAPED_TEXT_COLUMN, escaped_text)
     end
   end
 end
